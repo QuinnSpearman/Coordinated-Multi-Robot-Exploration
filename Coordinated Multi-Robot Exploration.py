f626dc479d4d3d048a53c855cebf42a0998eb39f
QuinnSpearman <51034953+QuinnSpearman@users.noreply.github.com>
2020-10-19 23:54:20 -0400

    Multiple robots can navigate using hill-climbing
    + Bot class
    + Visited class
    + Wall class
    + Empty class
    + Hill climb search
    + Right click to place more bots
    + Press space bar to show hill-climbing
    
    Notes: Will most likely convert these classes into children of a square class.

--- Coordinated Multi-Robot Exploration.py
+++ Coordinated Multi-Robot Exploration.py
@@ -4,51 +4,163 @@
 # In[1]:
 
 
+import time
 import pygame as pg
+from scipy.spatial import distance
 pg.init()
 
 width = 800
 height = 600
-nSquares = 20
+nSquares = 40
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 yellow = (255, 255, 0)
+purple = (166, 0, 255)
+teal = (0, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+orange = (255, 127, 0)
 grey = (66, 66, 66)
+
+colors = [yellow, purple, red, teal, green, blue, orange]
 
 arrow = pg.image.load('arrow.png')
 #arrow = pg.transform.scale(arrow, ())
 
 gameDisplay = pg.display.set_mode((width, height))
 
+clock = pg.time.Clock()
+
 # fix self.margin
 
+#class Square:
+#    def __init__(self):
+#        self.color
+#        
+#    def set_color(color):
+#        this.color = color
+#        
+#    def display_square()
+
+#def hill_climb_search():
+        
+class Empty():
+    def __init__(self):
+        self.color = white
+        self.type = "empty"
+        
+    def display(self, square):
+        pg.draw.rect(gameDisplay, self.color, square)
+        
+    def get_type(self):
+        return self.type
+    
+class Wall():
+    def __init__(self):
+        self.color = grey
+        self.type = "wall"
+        
+    def display(self, square):
+        pg.draw.rect(gameDisplay, self.color, square)
+        
+    def get_type(self):
+        return self.type
+        
+class Visited():
+    def __init__(self, color):
+        self.color = color
+        self.type = "visited"
+        
+    def get_type(self):
+        return self.type
+    
+    def display(self, square):
+        pg.draw.rect(gameDisplay, self.color, square)
+        
+
+class Bot():
+    def __init__(self, number, margin, position):
+        self.margin = margin
+        self.x = position[0]
+        self.y = position[1]
+        self.position = position
+        self.number = number
+        self.color = self.set_color(number)
+        self.type = "bot"
+        self.direction = 270
+        self.arrow = pg.transform.scale(arrow, (margin, margin))
+        self.arrow = pg.transform.rotate(self.arrow, 90)
+        self.actions = {"left": False, "right": True, "up": False, "down": True}
+        
+    def set_color(self, number):
+        return colors[number]
+    
+    def get_color(self):
+        return self.color
+    
+    def get_actions(self):
+        return self.actions
+    
+    def get_arrow(self):
+        return self.arrow
+    
+    def get_type(self):
+        return self.type
+    
+    def get_number(self):
+        return self.number
+    
+    def get_position(self):
+        return self.position
+    
+    def set_actions(self, actions):
+        self.actions = actions
+        
+    def set_position(self, position):
+        self.x = position[0]
+        self.y = position[1]
+    
+    def set_direction(self, newAngle):
+        self.arrow = pg.transform.rotate(self.arrow, newAngle - self.direction)
+        self.direction = newAngle
+        
+    def display(self, square):
+        pg.draw.rect(gameDisplay, self.color, square)
+        gameDisplay.blit(self.arrow, (self.margin * self.x + 1, self.margin * self.y + 1))
+        
+        
 class Grid:
     
     def __init__(self, width, height, nSquares):
         self.width = width
         self.height = height
         self.nSquares = nSquares
-        self.squares = [["empty" for y in range(int(height / (width / nSquares)))] for x in range(nSquares)]
+        self.squares = [[Empty() for y in range(int(height / (width / nSquares)))] for x in range(nSquares)]
         
         for x in range(len(self.squares)):
-            self.squares[x][0] = "wall"
-            self.squares[x][len(self.squares[0]) - 1] = "wall"
+            self.squares[x][0] = Wall()
+            self.squares[x][len(self.squares[0]) - 1] = Wall()
             
         for y in range(len(self.squares[0])):
-            self.squares[0][y] = "wall"
-            self.squares[len(self.squares) - 1][y] = "wall"
+            self.squares[0][y] = Wall()
+            self.squares[len(self.squares) - 1][y] = Wall()
             
-        self.arrowPosition = (1, 1)    
-        self.squares[self.arrowPosition[0]][self.arrowPosition[1]] = "bot"
-        self.arrowDirection = 270
-        self.margin = int(self.width / nSquares)
-        self.arrow = pg.transform.scale(arrow, (self.margin, self.margin))
-        self.arrow = pg.transform.rotate(self.arrow, 90)
-        self.actions = {"left": False, "right": True, "up": False, "down": True}
+        self.botLocations = []    
+        self.margin = int(self.width / nSquares)    
+        #self.arrowPosition = (1, 1) 
+        self.botCount = 0
+        self.squares[1][1] = Bot(self.botCount, self.margin, (1, 1))
+        self.botLocations.append((1, 1))
+        self.botCount += 1
+        self.goal = (6, 6)
         
-    def create_rect(self, x, y):po
+        #self.arrow = pg.transform.scale(arrow, (self.margin, self.margin))
+        #self.arrow = pg.transform.rotate(self.arrow, 90)
+        #self.actions = {"left": False, "right": True, "up": False, "down": True}
+        
+    def create_rect(self, x, y):
         w = (x * self.margin) + 2
         h = (y * self.margin) + 2
         return [w, h, self.margin - 2, self.margin - 2]
@@ -64,73 +176,166 @@ class Grid:
         
         for x in range(nSquares):
             for y in range(int(height / (width / nSquares))):
-                if(self.squares[x][y] == "bot"):
-                    square = self.create_rect(x, y)
-                    pg.draw.rect(gameDisplay, yellow, square)
-                    gameDisplay.blit(self.arrow, (self.margin * x + 1, self.margin * y + 1))
-                elif(self.squares[x][y] == "wall"):
-                    square = self.create_rect(x, y)
-                    pg.draw.rect(gameDisplay, grey, square)
-                elif(self.squares[x][y] == "visited"):
-                    square = self.create_rect(x, y)
-                    pg.draw.rect(gameDisplay, yellow, square)
+                square = self.create_rect(x, y)
+                self.squares[x][y].display(square)
+                
+
+                #if(self.squares[x][y] == "empty"):
+                #    pass
+                    #square = self.create_rect(x, y)
+                    #pg.draw.rect(gameDisplay, yellow, square)
+                    #gameDisplay.blit(self.arrow, (self.margin * x + 1, self.margin * y + 1))
+                #elif(self.squares[x][y] == "wall"):
+                #    square = self.create_rect(x, y)
+                #    pg.draw.rect(gameDisplay, grey, square)
+                #elif(self.squares[x][y] == "visited"):
+                #    square = self.create_rect(x, y)
+                #    pg.draw.rect(gameDisplay, yellow, square)
+                #else:  #if(self.squares[x][y] == "bot"):
+                #    square = self.create_rect(x, y)
+                #    pg.draw.rect(gameDisplay, yellow, square)
+                #    gameDisplay.blit(self.squares[x][y].get_arrow(), (self.margin * x + 1, self.margin * y + 1))
                 
     def set_wall(self, coordinates):
-        x = int(coordinates[0] / 40)
-        y = int(coordinates[1] / 40)
+        x = int(coordinates[0] / self.margin)
+        y = int(coordinates[1] / self.margin)
         
-        if self.squares[x][y] == "empty":
-            self.squares[x][y] = "wall"
-        elif self.squares[x][y] == "wall":
-            self.squares[x][y] = "empty"
-        
-    def rotate_arrow(self, newAngle):
-        self.arrow = pg.transform.rotate(self.arrow, newAngle - self.arrowDirection)
-        self.arrowDirection = newAngle
-        
-    def move_arrow(self, direction):
-        if self.actions[direction]:
+        if self.squares[x][y].get_type() == "empty":
+            self.squares[x][y] = Wall()
+        elif self.squares[x][y].get_type() == "wall":
+            self.squares[x][y] = Empty() 
             
-            self.squares[self.arrowPosition[0]][self.arrowPosition[1]] = "visited"
+    def set_bot(self, coordinates):
+
+        x = int(coordinates[0] / self.margin)
+        y = int(coordinates[1] / self.margin)
+        
+        if self.squares[x][y].get_type() == "empty" or self.squares[x][y].get_type() == "wall":
+            self.squares[x][y] = Bot(self.botCount, self.margin, (x, y))
+            self.botLocations.append(self.squares[x][y].get_position())
+            self.botCount += 1        
+            
+    #def rotate_arrow(self, newAngle):
+    #    self.arrow = pg.transform.rotate(self.arrow, newAngle - self.arrowDirection)
+    #    self.arrowDirection = newAngle
+    
+    def get_bot_position(number):
+        for x in range(len(self.squares)):
+            for y in range(len(self.squares[0])):
+                if self.squares[x][y].get_type() == "bot":
+                    if self.squares[x][y].get_number() == number:
+                        return (x, y)
+                
+        
+    def move_bot(self, direction, number):
+        
+        position = self.botLocations[number]
+        
+        x = position[0]
+        y = position[1]
+        
+        actions = self.squares[x][y].get_actions()
+        
+        if actions[direction]:
+            
+            tempBot = self.squares[x][y]
+            
+            self.squares[x][y] = Visited(tempBot.get_color())
             
             if direction == "left":
-                self.squares[self.arrowPosition[0] - 1][self.arrowPosition[1]] = "bot"
-                self.arrowPosition = (self.arrowPosition[0] - 1, self.arrowPosition[1])
-            elif direction == "right":
-                self.squares[self.arrowPosition[0] + 1][self.arrowPosition[1]] = "bot"
-                self.arrowPosition = (self.arrowPosition[0] + 1, self.arrowPosition[1])
-            elif direction == "up":
-                self.squares[self.arrowPosition[0]][self.arrowPosition[1] - 1] = "bot"
-                self.arrowPosition = (self.arrowPosition[0], self.arrowPosition[1] - 1)
-            elif direction == "down":
-                self.squares[self.arrowPosition[0]][self.arrowPosition[1] + 1] = "bot"
-                self.arrowPosition = (self.arrowPosition[0], self.arrowPosition[1] + 1)
+                tempBot.set_direction(90)
+                self.squares[x - 1][y] = tempBot
+                self.squares[x - 1][y].set_position((x - 1, y))
+                self.botLocations[number] = (x - 1, y)
+                self.update_actions((x - 1, y))
                 
+            elif direction == "right":
+                tempBot.set_direction(270)
+                self.squares[x + 1][y] = tempBot
+                self.squares[x + 1][y].set_position((x + 1, y))
+                self.botLocations[number] = (x + 1, y)
+                self.update_actions((x + 1, y))
+                
+            elif direction == "up":
+                tempBot.set_direction(0)
+                self.squares[x][y - 1] = tempBot
+                self.squares[x][y - 1].set_position((x, y - 1))
+                self.botLocations[number] = (x, y - 1)
+                self.update_actions((x, y - 1))
+
+            elif direction == "down":
+                tempBot.set_direction(180)
+                self.squares[x][y + 1] = tempBot
+                self.squares[x][y + 1].set_position((x, y + 1))
+                self.botLocations[number] = (x, y + 1)
+                self.update_actions((x, y + 1))
+                
+        #self.update_actions((x, y))
+                           
             
-    def update_actions(self):
-        x = self.arrowPosition[0]
-        y = self.arrowPosition[1]
+    def update_actions(self, position):
+        x = position[0]
+        y = position[1]
         
-
-        if self.squares[x - 1][y] != "wall":
-            self.actions["left"] = True
+        actions = {"left": False, "right": False, "up": False, "down": False}
+        
+        if self.squares[x - 1][y].get_type() not in ["wall", "bot"]:
+            actions["left"] = True
         else:
-            self.actions["left"] = False 
+            actions["left"] = False 
 
-        if self.squares[x + 1][y] != "wall":
-            self.actions["right"] = True
+        if self.squares[x + 1][y].get_type() not in ["wall", "bot"]:
+            actions["right"] = True
         else:
-            self.actions["right"] = False
+            actions["right"] = False
             
-        if self.squares[x][y - 1] != "wall":
-            self.actions["up"] = True
+        if self.squares[x][y - 1].get_type() not in ["wall", "bot"]:
+            actions["up"] = True
         else:
-            self.actions["up"] = False    
+            actions["up"] = False    
           
-        if self.squares[x][y + 1] != "wall":
-            self.actions["down"] = True
+        #print(x, y)
+        if self.squares[x][y + 1].get_type() not in ["wall", "bot"]:
+            actions["down"] = True
         else:
-            self.actions["down"] = False
+            actions["down"] = False
+            
+        self.squares[x][y].set_actions(actions)
+        
+    def get_best_action(self, position):
+        x = position[0]
+        y = position[1]
+        print(position)
+        actions = self.squares[x][y].get_actions()
+        print(actions)
+        
+        possibleActions = {"left": 1000000, "right": 1000000, "up": 1000000, "down": 1000000}
+        
+        if actions["left"]:
+            #possibleActions.append({"left": distance.euclidean((x - 1, y), self.goal)})
+            possibleActions["left"] = distance.euclidean((x - 1, y), self.goal)
+            
+        if actions["right"]:
+            #possibleActions.append({"right": distance.euclidean((x + 1, y), self.goal)})
+            possibleActions["right"] = distance.euclidean((x + 1, y), self.goal)
+            
+        if actions["up"]:
+            #possibleActions.append({"up": distance.euclidean((x, y - 1), self.goal)})
+            possibleActions["up"] = distance.euclidean((x, y - 1), self.goal)
+            
+        if actions["down"]:
+            #possibleActions.append({"down": distance.euclidean((x, y + 1), self.goal)})
+            possibleActions["down"] = distance.euclidean((x, y + 1), self.goal)
+            
+        keys = list(possibleActions.keys())
+        values = list(possibleActions.values())
+        #print(possibleActions)
+        #print(keys)
+        #print(values)
+        
+        #print(keys[values.index(min(values))])
+            
+        return keys[values.index(min(values))]
             
 grid = Grid(width, height, nSquares)
 
@@ -138,42 +343,75 @@ pg.display.set_caption('Coordinated Multi-Robot Exploration')
 
 pg.display.update()
 
-print(grid.actions)
+#print(grid.actions)
 
 gameExit = False
+startSearching = False
 
 while not gameExit:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             gameExit = True
         if event.type == pg.KEYDOWN:
-            grid.update_actions()
-            if event.key == pg.K_LEFT:
-                grid.rotate_arrow(90)
-                grid.move_arrow("left")
-            if event.key == pg.K_RIGHT:
-                grid.rotate_arrow(270)
-                grid.move_arrow("right")
-            elif event.key == pg.K_UP:
-                grid.rotate_arrow(0)
-                grid.move_arrow("up")
-            elif event.key == pg.K_DOWN:
-                grid.rotate_arrow(180)
-                grid.move_arrow("down")
-        elif event.type == pg.MOUSEBUTTONUP:
+            if event.key == pg.K_SPACE:
+                startSearching = True
+                #grid.move_bot("right", 0)
+                #grid.move_bot("right", 1)
+        #    grid.update_actions()
+        #    if event.key == pg.K_LEFT:
+        #        grid.rotate_arrow(90)
+        #        grid.move_arrow("left")
+        #    if event.key == pg.K_RIGHT:
+        #        grid.rotate_arrow(270)
+        #        grid.move_arrow("right")
+        #    elif event.key == pg.K_UP:
+        #        grid.rotate_arrow(0)
+        #        grid.move_arrow("up")
+        #    elif event.key == pg.K_DOWN:
+        #        grid.rotate_arrow(180)
+        #        grid.move_arrow("down")
+        elif pg.mouse.get_pressed()[0]:
             grid.set_wall(pg.mouse.get_pos())
+        elif pg.mouse.get_pressed()[2]:
+            grid.set_bot(pg.mouse.get_pos())
+        
+        # right mouse click -> add bot
             
     gameDisplay.fill(white)
+    
+    if startSearching:
+    
+        botNum = 0
+        for botPosition in grid.botLocations:
+            #print(botPosition)
+            x = botPosition[0]
+            y = botPosition[1]
+            
+            grid.update_actions(botPosition)
+            
+            tempBot = grid.squares[x][y]
+            
+            directionToMove = grid.get_best_action(botPosition)
+            grid.move_bot(directionToMove, botNum)
+
+            botNum += 1
+            
+        time.sleep(0.5)
+                
+        
 
     grid.display()
     
     pg.display.update()
     
+    #time.sleep(0.5)
+    
+    
 pg.quit()
 quit()
 
 
-# In[ ]:
+# In[3]:
 
 
 
